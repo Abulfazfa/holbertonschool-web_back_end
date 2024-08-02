7ab020c25c51ea5e425080b02c4c4eaaca296c43
Abulfazfa <abulfazfa@code.edu.az>
2024-08-02 15:51:05 +0400

    lifo_update

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -13,6 +13,7 @@ class LIFOCache(BaseCaching):
 
     def put(self, key, item):
         """ Add an item in the cache """
+
         if key and item:
             self.cache_data[key] = item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
@@ -22,6 +23,6 @@ class LIFOCache(BaseCaching):
 
     def get(self, key):
         """ Get an item by key """
+        
         if key in self.cache_data:
             return self.cache_data[key]
-            
