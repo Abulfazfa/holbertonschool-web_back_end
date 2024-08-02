b90cfda5e3b3f769b6ebc40cd10c3dbdf5b01879
Abulfazfa <abulfazfa@code.edu.az>
2024-08-02 15:32:09 +0400

    lifoalgr

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -3,26 +3,26 @@
 
 from base_caching import BaseCaching
 
-
-class LIFOCache (BaseCaching):
+class LIFOCache(BaseCaching):
     """ LIFOCache class"""
 
     def __init__(self):
-        """ Initiliaze """
+        """ Initialize """
         super().__init__()
 
     def put(self, key, item):
         """ Add an item in the cache """
-        if key and item:
-            self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                last_item = next(reversed(self.cache_data))
-                print("DISCARD: {}".format(last_item))
-                self.cache_data.pop(last_item)
-                self.cache_data = reversed(self.cache_data)
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # Get the last added item (LIFO)
+            last_key = next(reversed(self.cache_data))
+            print(f"DISCARD: {last_key}")
+            self.cache_data.pop(last_key)
 
     def get(self, key):
-        """ Get an item """
-        if key in self.cache_data:
-            return self.cache_data[key]
-
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
