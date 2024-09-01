1a5ae12ab95cc195360c18935f02f0c78c03678c
Abulfazfa <abulfazfa@code.edu.az>
2024-09-01 11:34:02 +0400

    update task 2

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -6,29 +6,23 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
-
 class Config(object):
-    """Setups"""
+    """Setup"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-
 app.config.from_object(Config)
-
-
-@app.route('/')
-def home():
-    """ Home Page
-    """
-    return render_template('2-index.html')
-
 
 @babel.localeselector
 def get_locale():
-    """Get locale"""
+    """Determine the best match language"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+@app.route('/')
+def home():
+    """Home Page"""
+    return render_template('1-index.html')
 
 if __name__ == "__main__":
     app.run("0.0.0.0", "5000")
