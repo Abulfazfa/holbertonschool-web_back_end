7407f3ca0dc3b8dbc0943f08ca71567075cd5957
Abulfazfa <abulfazfa@code.edu.az>
2024-09-01 11:39:05 +0400

    update task 2

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,28 +1,42 @@
 #!/usr/bin/env python3
-"""Base flask app"""
+"""
+This module sets up a Flask application with Babel for internationalization.
+"""
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
 
-class Config(object):
-    """Setup"""
+class Config:
+    """
+    Config class to set available languages, default locale, and timezone.
+    """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
+app = Flask(__name__)
 app.config.from_object(Config)
 
+babel = Babel(app)
+
+
 @babel.localeselector
-def get_locale():
-    """Determine the best match language"""
+def get_locale() -> str:
+    """
+    Accept-Language
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
-def home():
-    """Home Page"""
-    return render_template('1-index.html')
+def index() -> str:
+    """
+    The index route that returns the rendered index.html template.
+    """
+    return render_template('2-index.html')
+
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", "5000")
+    app.run()
