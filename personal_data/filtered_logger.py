f5cf1d256a71525d04661751d686ee923a7eb020
Abulfazfa <abulfazfa@code.edu.az>
2024-09-02 16:08:58 +0400

    regex

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,9 +4,7 @@
 import re
 from typing import List
 
-
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """ Filtering  fields """   
-    for item in fields:
-        message = re.sub(f"{item}=.*?{separator}", f"{item}={redaction}{separator}", message)
-    return message
+    """Filtering fields"""
+    pattern = '|'.join(f"{field}=" for field in fields)
+    return re.sub(fr'(?<={pattern})(.*?)(?={separator})', redaction, message)
