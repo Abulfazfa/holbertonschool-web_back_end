623b058013bde0599d586ce0991056faf462600b
Abulfazfa <abulfazfa@code.edu.az>
2024-09-02 16:04:36 +0400

    regex

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" filtered logger """
+""" Filtered logger """
 
 import re
 from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    '''Filtering  fields'''
+    """ Filtering  fields """   
     for item in fields:
         message = re.sub(f"{item}=.*?{separator}", f"{item}={redaction}{separator}", message)
     return message
