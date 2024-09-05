a82ac26d1659545bc85c234da121aa6c2c3c1a52
Abulfazfa <abulfazfa@code.edu.az>
2024-09-05 16:32:16 +0400

    auth class update

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -22,23 +22,27 @@ elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler
+    """ unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden handler
+    """ forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
 
 @app.before_request
 def before_request() -> str:
@@ -50,15 +54,16 @@ def before_request() -> str:
     pathes = ['/api/v1/status/',
               '/api/v1/unauthorized/',
               '/api/v1/forbidden/'
-            ]
+              ]
 
-    if not auth.require_auth(request.path, pathes):
+    if not (auth.require_auth(request.path, pathes)):
         return
 
     if (auth.authorization_header(request)) is None:
         abort(401)
     if (auth.current_user(request)) is None:
         abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
