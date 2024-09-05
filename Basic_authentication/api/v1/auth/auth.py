79d84a827330049c076f42812199a522ac53db9f
Abulfazfa <abulfazfa@code.edu.az>
2024-09-05 16:04:45 +0400

    Request validation

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -15,12 +15,31 @@ class Auth:
         """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method to check if a path requires authentication"""
-        return False
+        """ require_auth method that returns
+        True if the path is not in the list
+        """
+
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path[-1] is not '/':
+            path += '/'
+
+        for paths in excluded_paths:
+            if paths.endswith('*'):
+                if path.startswith(paths[:-1]):
+                    return False
+            elif path == paths:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method to get the authorization header from a request"""
-        return None
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Method to get the current user from a request """    
