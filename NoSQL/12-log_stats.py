79852d3b1e918575bf39d1430e6644dce37e54fd
Abulfazfa <abulfazfa@code.edu.az>
2024-09-20 14:51:52 +0400

    merger

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,7 +4,8 @@ from pymongo import MongoClient
 
 
 def main():
-    """Main function that provides some stats about Nginx logs stored in MongoDB."""
+    """Main function that provides some stats about
+    Nginx logs stored in MongoDB."""
     client = MongoClient("mongodb://localhost:27017/")
     db = client.logs
     collection = db.nginx
@@ -14,8 +15,8 @@ def main():
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
-        method_count = collection.count_documents({"method":method})
+        method_count = collection.count_documents({"method": method})
         print(f"method {method}: {method_count}")
-
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    status_check = (collection
+                    .count_documents({"method": "GET", "path": "/status"}))
     print(f"{status_check} status check")
