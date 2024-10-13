020783c009ccf76c7c6086ffa331d83fca3bb7da
Abulfazfa <abulfazfa@code.edu.az>
2024-10-13 11:27:57 +0400

    lint_fixing

--- Node_JS_basic/.eslintrc.js
+++ Node_JS_basic/.eslintrc.js
@@ -1,33 +1,19 @@
 module.exports = {
-    'env': {
-        'browser': true,      // Keep this if you're also targeting browser code
-        'node': true,         // Add this line to support Node.js globals
-        'es2021': true
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
     },
-    'extends': 'eslint:recommended',
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
+    "extends": [
+        "airbnb-base"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
     },
-    'parserOptions': {
-        'ecmaVersion': 12
+    "parserOptions": {
+        "ecmaVersion": 2018
     },
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
+    "rules": {
     }
 };
