63ba744515bad2bc53a0d758c20aa9f2b86d0e47
Abulfazfa <abulfazfa@code.edu.az>
2024-10-13 10:48:39 +0400

    lint_fixing

--- Node_JS_basic/.eslintrc.js
+++ Node_JS_basic/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     'env': {
-        'browser': true,
+        'browser': true,      // Keep this if you're also targeting browser code
+        'node': true,         // Add this line to support Node.js globals
         'es2021': true
     },
     'extends': 'eslint:recommended',
